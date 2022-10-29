111c2316e9a518cdfb1d292834fde0eb84f5c6a9
Владимир лазарев <vova.lazarev199@gmail.com>
2022-10-29 09:26:32 +0500

    Добавьте файлы проекта.

---
+++ Account/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+
+    public delegate void AccountHandler(string message);
+    public class AccountCl
+    {
+        int sum;
+        string fio;
+        AccountHandler taken;
+        public AccountCl(int sum,string fio)
+        {
+            this.sum = sum;
+            this.fio = fio;
+        }
+        // Регистрируем делегат
+        public void RegisterHandler(AccountHandler del)
+        {
+            taken += del;
+        }
+        // Отмена регистрации делегата
+        public void UnregisterHandler(AccountHandler del)
+        {
+            taken -= del; // удаляем делегат
+        }
+        public void Add(int sum) => this.sum += sum;
+        public void Take(int sum)
+        {
+            if (this.sum >= sum)
+            {
+                this.sum -= sum;
+                taken?.Invoke($"Со счета списано {sum} у.е.");
+            }
+            else
+                taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+        }
+    }
+}
---
+++ delegat/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Account;
+
+
+
+namespace delegat
+{
+    public partial class Form1 : Form
+    {
+        AccountCl account;
+        class Mani
+        {
+            
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            account = new AccountCl(Convert.ToInt32(textBox2),Convert.ToString(textBox1));
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            account.Add(Convert.ToInt32(textBox1));
+            listBox1.Items.Add(account);
+        }
+    }
+}
